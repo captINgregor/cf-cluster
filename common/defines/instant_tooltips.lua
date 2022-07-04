4edea5d8d45e6a3c3bc201529d30e43a9cca386b
Kaiser <51278032+captINgregor@users.noreply.github.com>
2022-07-04 08:50:24 +0800

    im stuff

---
+++ common/defines/instant_tooltips.lua
@@ -0,0 +1,3 @@
+NDefines_Graphics.NGraphics.TOOLTIP_DELAYED_DELAY = 0 				--How long before showing delayed tooltip.
+NDefines_Graphics.NGraphics.TOOLTIP_SHOW_DELAY = 0 					--How long before showing delayed tooltip.
+NDefines_Graphics.NGraphics.TOOLTIP_HIDE_DELAY = 0 					--How long before showing delayed tooltip.
